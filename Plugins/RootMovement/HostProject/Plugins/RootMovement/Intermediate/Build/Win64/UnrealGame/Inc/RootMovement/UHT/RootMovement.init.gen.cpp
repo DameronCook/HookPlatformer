89f8584c484c53c37aa065ebb366cae14c7de8a7
DameronCook <dameron.cook2303@gmail.com>
2025-02-19 10:48:52 -0600

    Level Changes

--- Plugins/RootMovement/HostProject/Plugins/RootMovement/Intermediate/Build/Win64/UnrealGame/Inc/RootMovement/UHT/RootMovement.init.gen.cpp
+++ Plugins/RootMovement/HostProject/Plugins/RootMovement/Intermediate/Build/Win64/UnrealGame/Inc/RootMovement/UHT/RootMovement.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeRootMovement_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x0E6A0F2C,
+				0x4D908658,
 				0xF27EDB90,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodeRootMovement_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_RootMovement.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_RootMovement(Z_Construct_UPackage__Script_RootMovement, TEXT("/Script/RootMovement"), Z_Registration_Info_UPackage__Script_RootMovement, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x0E6A0F2C, 0xF27EDB90));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_RootMovement(Z_Construct_UPackage__Script_RootMovement, TEXT("/Script/RootMovement"), Z_Registration_Info_UPackage__Script_RootMovement, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x4D908658, 0xF27EDB90));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
